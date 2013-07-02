a0064d39ed15506136faac63c832b0895d0c2fa8
etaypere <etaypere@gmail.com>
2013-07-03 00:52:13 +0300

    jquery component, simplest server

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', 80);
+app.use(express.favicon());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname)));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
